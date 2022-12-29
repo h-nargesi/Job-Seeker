2695024670ca7f5ec25cb9d83481dfc0aea24653
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2022-12-29 19:26:36 +0330

    debug logging on development

--- core-decision-dotnet/Program.cs
+++ core-decision-dotnet/Program.cs
@@ -4,10 +4,12 @@ using Photon.JobSeeker;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var file_event_level = builder.Environment.IsDevelopment() ? LogEventLevel.Debug : LogEventLevel.Information;
+
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console(Serilog.Events.LogEventLevel.Debug)
-    .WriteTo.File(builder.Configuration["Logging:FilePath"].ToString(), LogEventLevel.Information, rollingInterval: RollingInterval.Day)
+    .WriteTo.File(builder.Configuration["Logging:FilePath"].ToString(), file_event_level, rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
 Log.Information("Starting up ...");
