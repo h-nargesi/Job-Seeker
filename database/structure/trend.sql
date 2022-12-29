72e0241462babeb45819d6e9dac99fc05c92a86f
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2022-12-29 08:55:39 +0330

    decoration

--- database/structure/trend.sql
+++ database/structure/trend.sql
@@ -1,9 +1,9 @@
 create table Trend (
 	TrendID			integer 	not null	primary key,
 	AgencyID		integer		not null,
-	LastActivity	timestamp	not null	default current_timestamp,
 	Type			text		not null,
 	State			text		not null,
+	LastActivity	timestamp	not null	default current_timestamp,
 	
 	unique			(AgencyID, Type),
 	foreign key		(AgencyID) references Agency (AgencyID) on delete no action
