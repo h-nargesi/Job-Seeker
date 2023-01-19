3f20931e7fbaaa0cc1ba6b155772fa6219f5faff
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2023-01-19 15:40:02 +0330

    fixing bug

--- core-decision-dotnet/wwwroot/scripts/client-operations.js
+++ core-decision-dotnet/wwwroot/scripts/client-operations.js
@@ -2,13 +2,13 @@ function select_job(query) {
     
     document.querySelectorAll('tr.Selected').forEach(function (item) {
         item.classList.remove('Selected');
-        item.previousElementSibling.classList.remove('Selected-pr');
+        item.previousElementSibling?.classList.remove('Selected-pr');
     });
 
     if (!query) return;
 
     document.querySelectorAll('tr' + query).forEach(function (item) {
         item.classList.add('Selected');
-        item.previousElementSibling.classList.add('Selected-pr');
+        item.previousElementSibling?.classList.add('Selected-pr');
     });
 }
