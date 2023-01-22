c3cc7a6caa6df33c51e85570797fdbeca6bdfeee
hamedna <hamedna@systemgroup.net>
2023-01-22 09:54:16 +0330

    simplfy job-option

--- database/structure/job-option.sql
+++ database/structure/job-option.sql
@@ -19,32 +19,32 @@ values
 	--	Programming languages
 	 	(90,	'field',	'C#.NET',			'\basp\.net\b|\bc# ?\.net\b|\bc#|(\bdot ?|\.)net\b', null)
 	,	(80,	'field',	'Java',				'\b(java|jvm)\b', null)
-	,	(40,	'field',	'GO-Lang',			'\b(go[- ]?lang|(?-i)G[Oo])\b', null)
+	,	(30,	'field',	'GO-Lang',			'\b(go[- ]?lang|(?-i)G[Oo])\b', null)
 	,	(60,	'field',	'Frontend',			'\b(angular|javascript|typescript|jquery|client[- ]side script(ing)?)\b', null)
 	,	(90,	'field',	'Expert-SQL',		'\b(oracle|sql server|pl[- /]?sql|t[/-]?sql|ms[- ]?sql|sql|database)\b', null)
 	,	(10,	'field',	'Low-Level',		'\b(html|css|json)\b', null)
 	,	(10,	'field',	'Machine-Learning',	'\b((machine|deep)[- ]learning|natural[- ]network|(?-i)ML|(?-i)AI)\b', null)
-	,	(20,	'field',	'Python',			'\bpython\b', null)
+	,	(30,	'field',	'Python',			'\bpython\b', null)
 
 	--	Technologies
 	,	(10,	'tech',		'Web-API',			'\b((web[- ]?)?api|web services)\b', null)
-	,	(10,	'tech',		'TensorFlow',		'\b(tensor[- ]?flow)\b', null)
+	,	(10,	'tech',		'TensorFlow',		'\btensor[- ]?flow\b', null)
 	,	(05,	'tech',		'Git',				'\bgit\b', null)
 
 	--	Company Production
 	,	(30,	'production',	'ERP',			'\berp\b', null)
 
 	--	Company Benefits
-	,	(150,	'benefit',	'Relocation',		'\brelocation(\s+package)?\b|\bvisa(\s+(support|sponsorship))\b', null)
+	,	(150,	'benefit',	'Relocation',		'\brelocation(\s+(support|package))?\b|\bvisa(\s+(support|sponsorship))\b', null)
 	,	(02,	'salary',	'Salary',			'\bsalary\b.*?(\d[\d,]*(000|k))(.+?\b(month|year)\b)?',
 				'{ "money": 1, "period": 3 }')
 
 	--	Keywords
 	,	(02,	'keywords',	'Full-stack',		'\bfull[- ]?stack\b', null)
 	,	(01,	'keywords',	'Developer',		'\bdeveloper\b', null)
-	,	(02,	'keywords',	'Mid-Level',		'\b((mid|medium)[- ]level)\b', null)
-	,	(02,	'keywords',	'Front-End',		'\b(front[- ]end)\b', null)
-	,	(02,	'keywords',	'Back-End',			'\b(back[- ]end)\b', null)
+	,	(02,	'keywords',	'Mid-Level',		'\b(mid|medium)[- ]level\b', null)
+	,	(02,	'keywords',	'Front-End',		'\bfront[- ]?end\b', null)
+	,	(02,	'keywords',	'Back-End',			'\bback[- ]?end\b', null)
 
 	--	Rejection
 	--,	(01,	'rejected',	'react',			'\b(react)\b', null)
