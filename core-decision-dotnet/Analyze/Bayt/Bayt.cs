542b33618cd845adbc779bb4d4d76e60d22df3a2
Ryan Nargesi <hamed.nargesi.jar@gmail.com>
2025-01-21 00:34:44 +0330

    bayt start

---
+++ core-decision-dotnet/Analyze/Bayt/Bayt.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Photon.JobSeeker.Bayt;
+
+class Bayt : Agency
+{
+    private static readonly object @lock = new();
+
+    private Location[] Locations { get; set; } = Array.Empty<Location>();
+
+
+    public override string Name => "Bayt";
+
+    public override Location[] SearchingMethodTitles => Locations;
+
+    internal string RunningUrl => Locations[RunningSearchingMethodIndex].Url;
+
+
+    public override string BaseUrl
+    {
+        get
+        {
+            var base_link = Link.Trim();
+            return base_link.EndsWith('/') ? base_link[..^1] : base_link;
+        }
+    }
+    public override string SearchLink => $"https://www.bayt.com/en/{LocationDomains[RunningSearchingMethodIndex].Url}/jobs/{SearchTitle}-jobs/";
+
+    public override Regex? JobAcceptabilityChecker => BaytPage.reg_job_no_longer_accepting;
+
+
+    public override string GetMainHtml(string html) => BaytPageJob.GetHtmlContent(html);
+
+    protected override void LoadSettings(dynamic? settings)
+    {
+        lock (@lock)
+        {
+            if (settings == null)
+            {
+                Locations = new Location[] { new() };
+                RunningSearchingMethodIndex = 0;
+            }
+            else
+            {
+                var running = (int)settings!.running;
+                if (RunningSearchingMethodIndex == running) return;
+
+                Locations = settings.locations.ToObject<Location[]>();
+                RunningSearchingMethodIndex = running;
+            }
+        }
+    }
+
+    protected override void RunningSearchingMethodChanged(int value)
+    {
+        var location = Uri.EscapeDataString(Locations[value].Url);
+
+        BaytPage.reg_search_location_url = new Regex(@$"/en/{location}/jobs", RegexOptions.IgnoreCase);
+    }
+
+    protected override IEnumerable<Type> GetSubPages()
+    {
+        return TypeHelper.GetSubTypes(typeof(BaytPage));
+    }
+}
---
+++ core-decision-dotnet/Analyze/Bayt/BaytPageJob.cs
@@ -0,0 +1,86 @@
+﻿using System.Web;
+using HtmlAgilityPack;
+using Serilog;
+
+namespace Photon.JobSeeker.Bayt;
+
+class BaytPageJob : BaytPage
+{
+    public override int Order => 10;
+
+    public BaytPageJob(Bayt parent) : base(parent) { }
+
+    public override TrendState TrendState => TrendState.Analyzing;
+
+    public override Command[]? IssueCommand(string url, string content)
+    {
+        if (!reg_job_view.IsMatch(url)) return null;
+
+        var job = LoadJob(url, content);
+
+        using var evaluator = new JobEligibilityHelper();
+        var state = evaluator.EvaluateJobEligibility(job, Parent.JobAcceptabilityChecker);
+
+        var commands = new List<Command>();
+
+        if (state == JobState.Attention)
+        {
+            // TODO: save link
+
+            // TODO: apply link
+        }
+
+        return commands.ToArray();
+    }
+
+    private Job LoadJob(string url, string html)
+    {
+        using var database = Database.Open();
+
+        var url_matched = reg_job_view.Match(url);
+        if (!url_matched.Success) throw new Exception($"Invalid job url ({parent.Name}).");
+
+        var code = url_matched.Groups[1].Value;
+        var job = database.Job.Fetch(parent.ID, code);
+        var filter = JobFilter.Title | JobFilter.Html | JobFilter.Content | JobFilter.Tries;
+
+        if (job == null)
+        {
+            job = new Job
+            {
+                AgencyID = parent.ID,
+                Code = code,
+                State = JobState.Saved,
+                Url = url_matched.Value,
+            };
+
+            filter = JobFilter.All;
+        }
+
+        var title_match = reg_job_title.Match(html);
+        if (!title_match.Success)
+            Log.Warning("Title not found ({0}, {1})", parent.Name, code);
+        else job.Title = HttpUtility.HtmlDecode(title_match.Groups[2].Value).Trim();
+
+        job.SetHtml(GetHtmlContent(html));
+
+        Log.Information("{0} Job: {1} ({2})", parent.Name, job.Title, job.Code);
+        database.Job.Save(job, filter);
+
+        if (job.Content?.Contains("Bayt does not provide services in your region") == true)
+            throw new Exception("Bayt does not provide services in your region.");
+
+        return job;
+    }
+
+    public static string GetHtmlContent(string html)
+    {
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+
+        var main_content = doc.DocumentNode.SelectNodes("//div[contains(@class,'jobsearch-ViewJobLayout-jobDisplay')]")?
+                                            .FirstOrDefault();
+
+        return main_content?.OuterHtml ?? html;
+    }
+}
